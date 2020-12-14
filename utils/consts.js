3743aee8ae93b126d099196a73dfccf2d7666d1f
aaron <aaron@ug.com>
2020-12-14 20:05:11 +0800

    opt

--- utils/consts.js
+++ utils/consts.js
@@ -4,7 +4,10 @@ export const NAVS = [
     { id: 'lol', name: '英雄联盟' },
     { id: 'kog', name: '王者荣耀' }
 ]
-export const NAVS_MAP = Object.fromEntries(NAVS.map(item => [item.id, item.name]))
+export const NAVS_MAP = {}
+for (let i = 0; i < NAVS.length; i++) {
+    NAVS_MAP[NAVS[i].id] = NAVS[i].name
+}
 
 export const NAV_HEIGHT = 46
 
@@ -14,4 +17,7 @@ export const TOURNAMENT_STAUS = [
     { id: 2, name: '进行中' },
     { id: 3, name: '已结束' }
 ]
-export const TOURNAMENT_STAUS_MAP = Object.fromEntries(TOURNAMENT_STAUS.map(item => [item.id, item.name]))
+export const TOURNAMENT_STAUS_MAP = {}
+for (let i = 0; i < TOURNAMENT_STAUS.length; i++) {
+    TOURNAMENT_STAUS_MAP[TOURNAMENT_STAUS[i].id] = TOURNAMENT_STAUS[i].name
+}
